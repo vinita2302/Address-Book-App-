9e643c71877b0f94a7e6d14a600c31362f25d75b
vinita patanker <vinitapatanker@gmail.com>
2025-02-27 20:43:55 +0530

    [vinita patanker] project set up

---
+++ Address_Book_App/src/main/java/com/spring/Address_Book_App/AddressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Address_Book_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
---
+++ Address_Book_App/src/test/java/com/spring/Address_Book_App/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Address_Book_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
