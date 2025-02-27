f80a23de8de711c0bed2b70fd0c360ff0ca70c9c
vinita patanker <vinitapatanker@gmail.com>
2025-02-27 20:54:40 +0530

    [vinita patanker] Add : Adress Book App files

---
+++ Address_Book_App/src/main/java/com/spring/Address_Book_App/dto/AddressBookDto.java
@@ -0,0 +1,32 @@
+package com.spring.Address_Book_App.dto;
+
+
+public class AddressBookDto {
+
+    private String name;
+    private Long phoneNumber;
+
+    public AddressBookDto() {}
+
+    public AddressBookDto(String name, Long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
