78f47d4facb437f3f36bec8376abe75a7124e3a3
vinita patanker <vinitapatanker@gmail.com>
2025-02-27 21:03:55 +0530

    [vinita patanker] Add : UC4_service_layer

--- Address_Book_App/src/main/java/com/spring/Address_Book_App/service/AddressBookService.java
+++ Address_Book_App/src/main/java/com/spring/Address_Book_App/service/AddressBookService.java
@@ -1,4 +1,33 @@
 package com.spring.Address_Book_App.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AddressBookService {
+
+    public ResponseEntity<String> getAllAddressBook() {
+        return new ResponseEntity<>("get all address book", HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> getAddressBookById(String id) {
+        return new ResponseEntity<>("get address book of id "+id, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> createAddressBook() {
+        return new ResponseEntity<>("create address book", HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateAddressBook(String id) {
+        return new ResponseEntity<>("update address book of id "+id, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> deleteAddressBook(String id) {
+        return new ResponseEntity<>("delete address book of id "+id, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> deleteAllAddressBook() {
+        return new ResponseEntity<>("delete all address book", HttpStatus.OK);
+    }
 }
