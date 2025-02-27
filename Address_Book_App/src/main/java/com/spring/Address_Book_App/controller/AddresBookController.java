b2659dd9c817f906403847e8e211dfc64a728dab
vinita patanker <vinitapatanker@gmail.com>
2025-02-27 22:14:30 +0530

    [vinita patanker] Add : UC8_custom_logging_using_active_profile

--- Address_Book_App/src/main/java/com/spring/Address_Book_App/controller/AddresBookController.java
+++ Address_Book_App/src/main/java/com/spring/Address_Book_App/controller/AddresBookController.java
@@ -1,153 +1,56 @@
-package com.spring.Address_Book_App.controller;
-
-
+package com.spring.Address_Book_App.service;
 
 import com.spring.Address_Book_App.dto.AddressBookDto;
 import com.spring.Address_Book_App.model.AddressBook;
-import com.spring.Address_Book_App.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import com.spring.Address_Book_App.service.AddressBookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-
-import org.springframework.web.bind.annotation.*;
-
-
+@Slf4j
 @RestController
 @RequestMapping("/address-book")
 public class AddresBookController {
-
 
     @Autowired
     AddressBookService addressBookService;
 
     @GetMapping(value = {"", "/"})
-
     public ResponseEntity<List<AddressBook>> getAllAddressBook(){
+        log.info("Get all address book");
         return addressBookService.getAllAddressBook();
     }
 
     @GetMapping("/{name}")
     public ResponseEntity<AddressBook> getAddressBookByName(@PathVariable String name){
+        log.info("Get address book by name");
         return addressBookService.getAddressBookByName(name);
     }
 
     @PostMapping("")
     public ResponseEntity<AddressBook> createAddressBook(@RequestBody AddressBookDto addressBookDto){
+        log.info("create address book using {}", addressBookDto);
         return addressBookService.createAddressBook(addressBookDto);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<AddressBook> updateAddressBook(@PathVariable String id, @RequestBody AddressBookDto addressBookDto){
+        log.info("update address book of id {}", id, "with new address book as", addressBookDto);
         return addressBookService.updateAddressBook(Long.parseLong(id), addressBookDto);
-
-    public ResponseEntity<String> getAllAddressBook(){
-        return addressBookService.getAllAddressBook();
-
-    @GetMapping(value = {"", "/"})
-
-    public String getAllAddressBook(){
-        return "get all address book";
-    }
-
-    @GetMapping("/{id}")
-    public String getAddressBookById(@PathVariable String id){
-        return "get address book of id "+id;
-    }
-
-    @PostMapping("")
-    public String createAddressBook(){
-        return "create address book";
-    }
-
-    @PutMapping("/{id}")
-    public String updateAddressBook(@PathVariable String id){
-        return "update address book of id "+id;
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteAddressBook(@PathVariable String id){
-        return "delete address book of id "+id;
-    }
-
-    @DeleteMapping(value = {"", "/"})
-    public String deleteAllAddressBook(){
-        return "delete all address book";
-    }
-
-}
-
-    public ResponseEntity<String> getAllAddressBook(){
-        return new ResponseEntity("get all address book", HttpStatus.OK);
-
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getAddressBookById(@PathVariable String id){
-
-        return addressBookService.getAddressBookById(id);
-
-        return new ResponseEntity("get address book of id "+id, HttpStatus.OK);
-
-    }
-
-    @PostMapping("")
-    public ResponseEntity<String> createAddressBook(){
-
-        return addressBookService.createAddressBook();
-
-        return new ResponseEntity("create address book", HttpStatus.OK);
-
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateAddressBook(@PathVariable String id){
-
-        return addressBookService.updateAddressBook(id);
-
-
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAddressBook(@PathVariable String id){
-
+        log.info("delete address book of id {}", id);
         return addressBookService.deleteAddressBook(Long.parseLong(id));
-
-
-        return addressBookService.deleteAddressBook(id);
-
-        return new ResponseEntity("delete address book of id "+id, HttpStatus.OK);
-
     }
 
     @DeleteMapping(value = {"", "/"})
     public ResponseEntity<String> deleteAllAddressBook(){
-
+        log.info("delete all address book");
         return addressBookService.deleteAllAddressBook();
     }
 
 }
-
-
-        return addressBookService.deleteAllAddressBook();
-    }
-
-}
-
-        return new ResponseEntity("delete all address book", HttpStatus.OK);
-    }
-
-}
-
-
-
--- Address_Book_App/src/main/java/com/spring/Address_Book_App/service/AddressBookService.java
+++ Address_Book_App/src/main/java/com/spring/Address_Book_App/service/AddressBookService.java
@@ -2,84 +2,75 @@ package com.spring.Address_Book_App.service;
 
 import com.spring.Address_Book_App.dto.AddressBookDto;
 import com.spring.Address_Book_App.model.AddressBook;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
+@Slf4j
 @Service
 public class AddressBookService {
 
     private final List<AddressBook> addressBooks = new ArrayList<>();
+    private final AtomicLong idCounter = new AtomicLong(1); // Generate unique IDs
 
     public ResponseEntity<List<AddressBook>> getAllAddressBook() {
-        return new ResponseEntity<>(addressBooks, HttpStatus.OK);
+        log.info("Fetching all address books");
+        return ResponseEntity.ok(addressBooks);
     }
 
     public ResponseEntity<AddressBook> getAddressBookByName(String name) {
-        for (AddressBook addressBook : addressBooks){
-            if (addressBook.getName().equals(name)) return new ResponseEntity<>(addressBook, HttpStatus.FOUND);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        log.info("Searching for address book by name: {}", name);
+        Optional<AddressBook> addressBook = addressBooks.stream()
+                .filter(ab -> ab.getName().equalsIgnoreCase(name))
+                .findFirst();
+
+        return addressBook.map(book -> new ResponseEntity<>(book, HttpStatus.FOUND))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
     public ResponseEntity<AddressBook> createAddressBook(AddressBookDto addressBookDto) {
-        AddressBook newAddressBook = new AddressBook(addressBookDto.getName(), addressBookDto.getPhoneNumber());
+        AddressBook newAddressBook = new AddressBook(idCounter.getAndIncrement(), addressBookDto.getName(), addressBookDto.getPhoneNumber());
         addressBooks.add(newAddressBook);
+        log.info("Created new address book: {}", newAddressBook);
         return new ResponseEntity<>(newAddressBook, HttpStatus.CREATED);
     }
 
     public ResponseEntity<AddressBook> updateAddressBook(Long id, AddressBookDto addressBookDto) {
-        for (int i=0; i<addressBooks.size(); i++){
-            if (addressBooks.get(i).getId() == id){
-                addressBooks.set(i, new AddressBook(addressBookDto.getName(), addressBookDto.getPhoneNumber()));
-                return new ResponseEntity<>(addressBooks.get(i), HttpStatus.OK);
+        log.info("Updating address book with ID: {}", id);
+        for (int i = 0; i < addressBooks.size(); i++) {
+            if (addressBooks.get(i).getId().equals(id)) {
+                AddressBook updatedBook = new AddressBook(id, addressBookDto.getName(), addressBookDto.getPhoneNumber());
+                addressBooks.set(i, updatedBook);
+                return new ResponseEntity<>(updatedBook, HttpStatus.OK);
             }
         }
+        log.warn("Address book with ID {} not found", id);
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     public ResponseEntity<String> deleteAddressBook(Long id) {
-        for (int i=0; i<addressBooks.size(); i++){
-            if (addressBooks.get(i).getId() == id){
-                addressBooks.remove(i);
-                return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
-            }
+        log.info("Deleting address book with ID: {}", id);
+        boolean removed = addressBooks.removeIf(ab -> ab.getId().equals(id));
+        if (removed) {
+            return ResponseEntity.ok("Deleted successfully");
         }
-        return new ResponseEntity<>("Unable to delete address book "+id, HttpStatus.NOT_FOUND);
+        log.warn("Unable to delete, ID {} not found", id);
+        return new ResponseEntity<>("Unable to delete address book " + id, HttpStatus.NOT_FOUND);
     }
 
     public ResponseEntity<String> deleteAllAddressBook() {
-        if (addressBooks.removeAll(addressBooks)) return new ResponseEntity<>("delete all address book", HttpStatus.OK);
-
-@Service
-public class AddressBookService {
-
-    public ResponseEntity<String> getAllAddressBook() {
-        return new ResponseEntity<>("get all address book", HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> getAddressBookById(String id) {
-        return new ResponseEntity<>("get address book of id "+id, HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> createAddressBook() {
-        return new ResponseEntity<>("create address book", HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> updateAddressBook(String id) {
-        return new ResponseEntity<>("update address book of id "+id, HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> deleteAddressBook(String id) {
-        return new ResponseEntity<>("delete address book of id "+id, HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> deleteAllAddressBook() {
-        return new ResponseEntity<>("delete all address book", HttpStatus.OK);
-
+        if (!addressBooks.isEmpty()) {
+            addressBooks.clear();
+            log.info("Deleted all address books");
+            return ResponseEntity.ok("Deleted all address books");
+        }
+        log.warn("No address books to delete");
+        return new ResponseEntity<>("No address books to delete", HttpStatus.NOT_FOUND);
     }
 }
